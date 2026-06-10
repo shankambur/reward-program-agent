67f6fc421b2dc915b3c3473bf9efd8d1f3277bf8
Santhiya Chakravarthi <santhiya@Santhiyas-MacBook-Pro.local>
2026-06-09 21:53:08 -0700

    removed unwanted files

--- utils/reranker.py
+++
@@ -1,7 +0,0 @@
-def limit_context_chunks(results_with_score, rerank_top_n):
-    sorted_results = sorted(
-        results_with_score,
-        key=lambda item: item[1]
-    )
-
-    return sorted_results[:rerank_top_n]
